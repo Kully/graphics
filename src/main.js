8c579bf6ba6fca92205c2b1089f29e023aefce4e
Kully <adam@plot.ly>
2021-09-19 15:53:17 -0700

    clean code: rename variables, enclose in functions

--- src/main.js
+++ src/main.js
@@ -71,37 +71,30 @@ let canvas2 = document.getElementById("my-canvas-2");
 let ctx2 = canvas2.getContext("2d");
 
 
-let keypressDisplay = document.getElementById("keypress-display");
-
-
-function clearScreen()
+function clearScreen(context, color)
 {
-    ctx.fillStyle = COLOR_WHITE;
-    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+    context.fillStyle = color;
+    context.fillRect(0, 0, WIDTH, HEIGHT);
 }
 
-function clearScreen_2()
-{
-    ctx2.fillStyle = COLOR_WHITE;
-    ctx2.fillRect(0, 0, WIDTH, HEIGHT);
-}
 
-function drawPlayer_2()
+function drawCanvas2(context)
 {
-    ctx2.fillStyle = PLAYER["color"];
-    ctx2.fillRect(PLAYER["x"], PLAYER["y"], PLAYER["w"], PLAYER["h"]);
-}
+    context.fillStyle = COLOR_WHITE;
+    context.fillRect(0, 0, WIDTH, HEIGHT);
 
-function drawBlock_2()
-{
+    context.fillStyle = PLAYER["color"];
+    context.fillRect(PLAYER["x"], PLAYER["y"], PLAYER["w"], PLAYER["h"]);
+
     for(let obj of LEVEL_OBJECTS)
     {
-        ctx2.fillStyle = obj["color"];
-        ctx2.fillRect(obj["x"], obj["y"], obj["w"], obj["h"]);
+        context.fillStyle = obj["color"];
+        context.fillRect(obj["x"], obj["y"], obj["w"], obj["h"]);
     }
 }
 
-function lookupVelocity(n)
+
+function veloLookup(n)
 {
     return FRAME_VELO_LOOKUP[n];
 }
@@ -109,107 +102,60 @@ function lookupVelocity(n)
 
 function movePlayer()
 {
-    if(CONTROLLER["ArrowLeft"] === 1)
-    {
-        PLAYER["x"] -= Math.cos(PLAYER["angle"] + VISION_SPREAD);
-        PLAYER["y"] -= Math.sin(PLAYER["angle"] + VISION_SPREAD);
-    }
-    if(CONTROLLER["ArrowRight"] === 1)
-    {
-        PLAYER["x"] += Math.cos(PLAYER["angle"] + VISION_SPREAD);
-        PLAYER["y"] += Math.sin(PLAYER["angle"] + VISION_SPREAD);
-    }
-    if(CONTROLLER["ArrowUp"] === 1)
-    {
-        PLAYER["x"] += Math.cos(PLAYER["angle"] + VISION_SPREAD - Math.PI / 2);
-        PLAYER["y"] += Math.sin(PLAYER["angle"] + VISION_SPREAD - Math.PI / 2);
-    }
-    if(CONTROLLER["ArrowDown"] === 1)
-    {
-        PLAYER["x"] -= Math.cos(PLAYER["angle"] + VISION_SPREAD - Math.PI / 2);
-        PLAYER["y"] -= Math.sin(PLAYER["angle"] + VISION_SPREAD - Math.PI / 2);
-    }
+    // move left
+    let x_velo_l = veloLookup(KEYDOWN_COUNTER["ArrowLeft"]);
+    PLAYER["x"] -= x_velo_l * Math.cos(PLAYER["angle"] + VISION_SPREAD);
+    PLAYER["y"] -= x_velo_l * Math.sin(PLAYER["angle"] + VISION_SPREAD);
+
+    // move right
+    let x_velo_r = veloLookup(KEYDOWN_COUNTER["ArrowRight"]);
+    PLAYER["x"] += x_velo_r * Math.cos(PLAYER["angle"] + VISION_SPREAD);
+    PLAYER["y"] += x_velo_r * Math.sin(PLAYER["angle"] + VISION_SPREAD);
+
+
+    // move forwards
+    let x_velo_u = veloLookup(KEYDOWN_COUNTER["ArrowUp"]);
+    PLAYER["x"] += x_velo_u * Math.cos(PLAYER["angle"] + VISION_SPREAD - Math.PI / 2);
+    PLAYER["y"] += x_velo_u * Math.sin(PLAYER["angle"] + VISION_SPREAD - Math.PI / 2);
+    
+    // move backwards
+    let x_velo_d = veloLookup(KEYDOWN_COUNTER["ArrowDown"]);
+    PLAYER["x"] -= x_velo_d * Math.cos(PLAYER["angle"] + VISION_SPREAD - Math.PI / 2);
+    PLAYER["y"] -= x_velo_d * Math.sin(PLAYER["angle"] + VISION_SPREAD - Math.PI / 2);
 }
+
 
 function rotatePlayer()
 {
-    if(CONTROLLER["KeyA"] === 1)
-    {
-        PLAYER["angle"] -= ROTATE_ANGLE;
-        PLAYER["angle"] %= (TWO_PI);
-    }
-    if(CONTROLLER["KeyD"] === 1)
-    {
-        PLAYER["angle"] += ROTATE_ANGLE;
-        PLAYER["angle"] %= (TWO_PI);
-    }
+    // counter-clockwise
+    let x_velo_keya = veloLookup(KEYDOWN_COUNTER["KeyA"]);
+    PLAYER["angle"] -= x_velo_keya * ROTATE_ANGLE;
+    PLAYER["angle"] %= (TWO_PI);
+
+    // clockwise
+    let x_velo_keyd = veloLookup(KEYDOWN_COUNTER["KeyD"]);
+    PLAYER["angle"] += x_velo_keyd * ROTATE_ANGLE;
+    PLAYER["angle"] %= (TWO_PI);
 }
 
-
 document.addEventListener("keydown", keydownHandler)
-
 document.addEventListener("keyup", keyupHandler)
 
 function gameLoop(e)
 {
     let max_frame = 5
-    keypressDisplay.innerHTML = "";
     for(let code of EVENT_CODES)
     {
         if(CONTROLLER[code] === 1)
-        {
-            KEYDOWN_COUNTER[code] = Math.min(
-                max_frame,
-                KEYDOWN_COUNTER[code] + 1
-            );
-
-            // display keys down in front-end
-            keypressDisplay.innerHTML += EVENTCODE_CHAR_LOOKUP[code];
-            keypressDisplay.innerHTML += " ";
-        }
+            KEYDOWN_COUNTER[code] = Math.min(max_frame, KEYDOWN_COUNTER[code] + 1);
         else
-        {
-            KEYDOWN_COUNTER[code] = Math.max(
-                0,
-                KEYDOWN_COUNTER[code] - 1
-            );
-        }
+            KEYDOWN_COUNTER[code] = Math.max(0, KEYDOWN_COUNTER[code] - 1);
     }
 
-    // move player left
-    let x_velo_l = lookupVelocity(KEYDOWN_COUNTER["ArrowLeft"]);
-    PLAYER["x"] -= x_velo_l * Math.cos(PLAYER["angle"] + VISION_SPREAD);
-    PLAYER["y"] -= x_velo_l * Math.sin(PLAYER["angle"] + VISION_SPREAD);
+    movePlayer();
+    rotatePlayer();
 
-    // move player right
-    let x_velo_r = lookupVelocity(KEYDOWN_COUNTER["ArrowRight"]);
-    PLAYER["x"] += x_velo_r * Math.cos(PLAYER["angle"] + VISION_SPREAD);
-    PLAYER["y"] += x_velo_r * Math.sin(PLAYER["angle"] + VISION_SPREAD);
-
-
-    // move player up
-    let x_velo_u = lookupVelocity(KEYDOWN_COUNTER["ArrowUp"]);
-    PLAYER["x"] += x_velo_u * Math.cos(PLAYER["angle"] + VISION_SPREAD - Math.PI / 2);
-    PLAYER["y"] += x_velo_u * Math.sin(PLAYER["angle"] + VISION_SPREAD - Math.PI / 2);
-    
-    // move player down
-    let x_velo_d = lookupVelocity(KEYDOWN_COUNTER["ArrowDown"]);
-    PLAYER["x"] -= x_velo_d * Math.cos(PLAYER["angle"] + VISION_SPREAD - Math.PI / 2);
-    PLAYER["y"] -= x_velo_d * Math.sin(PLAYER["angle"] + VISION_SPREAD - Math.PI / 2);
-
-    // rotate player ccw
-    let x_velo_keya = lookupVelocity(KEYDOWN_COUNTER["KeyA"]);
-    PLAYER["angle"] -= x_velo_keya * ROTATE_ANGLE;
-    PLAYER["angle"] %= (TWO_PI);
-
-    // rotate player cw
-    let x_velo_keyd = lookupVelocity(KEYDOWN_COUNTER["KeyD"]);
-    PLAYER["angle"] += x_velo_keyd * ROTATE_ANGLE;
-    PLAYER["angle"] %= (TWO_PI);
-
-
-    // draw to screen
-    clearScreen();
+    clearScreen(ctx, COLOR_WHITE);
     ctx.fillStyle = COLOR_FLOOR;
     ctx.fillRect(0, HEIGHT/2, WIDTH, HEIGHT / 2);
 
@@ -243,17 +189,14 @@ function gameLoop(e)
             }
             d += 1;
         }
-        x+=1;
+        x += 1;
         ctx.fillStyle = LEVEL_OBJECTS[objectPtr]["color"];
         let h = LEVEL_OBJECTS[0]["z"] / depthReached * (HEIGHT / DRAW_DISTANCE);
         let y_pos = (HEIGHT - h) / 2;
         ctx.fillRect(x, y_pos, 1, h);
     }
 
-    // draw bird's eye view
-    clearScreen_2();
-    drawPlayer_2();
-    drawBlock_2();
+    drawCanvas2(ctx2);
 }
 
 
